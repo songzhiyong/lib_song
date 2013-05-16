d7fcfd5f8a63f65dfcab006fd3a81fc0be295997
jerome <songzhiyong1121@gmail.com>
2013-05-16 13:27:14 +0800

    Signed-off-by: jerome <songzhiyong1121@gmail.com>

--- src/com/utils/device/NetUtils.java
+++ src/com/utils/device/NetUtils.java
@@ -3,6 +3,7 @@
  * 创建时间：2013-1-3
  */
 package com.utils.device;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -10,6 +11,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.provider.Settings;
 import com.example.lib_song.R;
+
 /**
  * 系统联网状态工具类
  * 
@@ -29,22 +31,25 @@ public class NetUtils {
 			builder.setIcon(android.R.drawable.ic_dialog_alert);
 			builder.setTitle(R.string.app_netstate);
 			builder.setMessage(R.string.app_nonet);
-			builder.setPositiveButton(R.string.app_set, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					Intent mIntent = null;
-					if (android.os.Build.VERSION.SDK_INT > 10) {
-						mIntent = new Intent(Settings.ACTION_SETTINGS);
-					} else {
-						mIntent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
-					}
-					context.startActivity(mIntent);
-				}
-			});
-			builder.setNegativeButton(R.string.app_exit, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();
-				}
-			});
+			builder.setPositiveButton(R.string.settings,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							Intent mIntent = null;
+							if (android.os.Build.VERSION.SDK_INT > 10) {
+								mIntent = new Intent(Settings.ACTION_SETTINGS);
+							} else {
+								mIntent = new Intent(
+										Settings.ACTION_WIRELESS_SETTINGS);
+							}
+							context.startActivity(mIntent);
+						}
+					});
+			builder.setNegativeButton(R.string.exit,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.cancel();
+						}
+					});
 			builder.create();
 			builder.show();
 		}
