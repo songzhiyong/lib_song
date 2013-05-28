948b9680a5324a2b9df15d7cb96bb8b52690a6ae
jerome <songzhiyong1121@gmail.com>
2013-05-28 16:22:55 +0800

    A

---
+++ framework/com/android/internal/R.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+
+package com.android.internal;
+
+/**
+ * 
+ */
+public final class R {
+	public static class styleable {
+		public static int[] ToggleButton = null;
+
+		public static int[] AbsSpinner;
+
+		public static int AbsSpinner_entries;
+
+		public static int[] Gallery;
+
+		public static int Gallery_gravity;
+
+		public static int Gallery_animationDuration;
+
+		public static int Gallery_spacing;
+
+		public static int Gallery_unselectedAlpha;
+
+		public static int Animation;
+
+		public static int Animation_duration;
+		public static int Animation_startOffset;
+
+		public static int Animation_fillEnable;
+		public static int Animation_fillBefore;
+		public static int Animation_fillAfter;
+
+		public static int Animation_interpolator;
+
+		public static int Animation_repeatCount;
+		public static int Animation_repeatMode;
+
+		public static int Animation_zAdjustment;
+
+		public static int Animation_detachWallpaper;
+
+		public static int Animation_fillEnabled;
+
+		public static int AlphaAnimation;
+		public static int AlphaAnimation_fromAlpha;
+		public static int AlphaAnimation_toAlpha;
+
+		public static int ToggleButton_textOn;
+
+		public static int ToggleButton_textOff;
+
+		public static int[] CompoundButton;
+
+		public static int CompoundButton_checked;
+	}
+
+	public static class layout {
+		public static int simple_spinner_item;
+
+		public static int simple_spinner_dropdown_item;
+
+	}
+
+	public static class attr {
+		public static int galleryStyle;
+
+	}
+
+	public static class style {
+		public static int Theme_Dialog_Alert;
+	}
+
+	public static class id {
+		public static int body;
+	}
+
+}
