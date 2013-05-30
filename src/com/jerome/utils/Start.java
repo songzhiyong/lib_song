4880545683d52a0cf679405a3c2b6ae4f5348b5a
jerome <songzhiyong1121@gmail.com>
2013-05-30 23:22:02 +0800

    a

--- src/com/jerome/utils/Start.java
+++ src/com/jerome/utils/Start.java
@@ -1,8 +1,10 @@
 package com.jerome.utils;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
 /**
  * 
  * Activity的启动器
@@ -10,8 +12,14 @@ import android.support.v4.app.Fragment;
  * @author SongZhiyong
  */
 public class Start {
+	public static final int MODE_LEFT_IN_RIGHT_OUT = 0;
+	public static final int MODE_RIGHT_IN_LEFT_OUT = 1;
+	public static final int MODE_UP_IN_BOTTOM_OUT = 2;
+	public static final int MODE_BOTTOM_IN_UP_OUT = 3;
+
 	// 不需要返回结果的启动新Activity,无请求码
 	public static final int REQUEST_CODE_INVALID = -1;
+
 	/**
 	 * Fragment启动Activity
 	 * 
@@ -23,6 +31,7 @@ public class Start {
 	public static void start(Fragment fragment, Class<?> cls) {
 		start(null, fragment, cls, null, REQUEST_CODE_INVALID, 0, 0);
 	}
+
 	/**
 	 * Fragment启动Activity(携带数据)
 	 * 
@@ -36,6 +45,7 @@ public class Start {
 	public static void start(Fragment fragment, Class<?> cls, Bundle extras) {
 		start(null, fragment, cls, extras, REQUEST_CODE_INVALID, 0, 0);
 	}
+
 	/**
 	 * Fragment启动Activity(forResult)
 	 * 
@@ -49,6 +59,7 @@ public class Start {
 	public static void start(Fragment fragment, Class<?> cls, int reqCode) {
 		start(null, fragment, cls, null, reqCode, 0, 0);
 	}
+
 	/**
 	 * Fragment启动Activity(携带数据，forResult)
 	 * 
@@ -61,9 +72,11 @@ public class Start {
 	 * @param reqCode
 	 *            请求码
 	 */
-	public static void start(Fragment fragment, Class<?> cls, Bundle extras, int reqCode) {
+	public static void start(Fragment fragment, Class<?> cls, Bundle extras,
+			int reqCode) {
 		start(null, fragment, cls, extras, reqCode, 0, 0);
 	}
+
 	/**
 	 * Activity启动Activity
 	 * 
@@ -75,6 +88,7 @@ public class Start {
 	public static void start(Activity activity, Class<?> cls) {
 		start(activity, null, cls, null, REQUEST_CODE_INVALID, 0, 0);
 	}
+
 	/**
 	 * Activity启动Activity(携带数据)
 	 * 
@@ -88,6 +102,7 @@ public class Start {
 	public static void start(Activity activity, Class<?> cls, Bundle extras) {
 		start(activity, null, cls, extras, REQUEST_CODE_INVALID, 0, 0);
 	}
+
 	/**
 	 * Activity启动Activity(forResult)
 	 * 
@@ -99,6 +114,7 @@ public class Start {
 	public static void start(Activity activity, Class<?> cls, int reqCode) {
 		start(activity, null, cls, null, reqCode, 0, 0);
 	}
+
 	/**
 	 * Activity启动Activity(携带数据，forResult)
 	 * 
@@ -109,9 +125,11 @@ public class Start {
 	 * @param extras
 	 *            携带数据的bundle
 	 */
-	public static void start(Activity activity, Class<?> cls, Bundle extras, int reqCode) {
+	public static void start(Activity activity, Class<?> cls, Bundle extras,
+			int reqCode) {
 		start(activity, null, cls, extras, reqCode, 0, 0);
 	}
+
 	/**
 	 * Activity启动Activity(动画效果)
 	 * 
@@ -124,9 +142,12 @@ public class Start {
 	 * @param exitAnim
 	 *            原Activity退出动画
 	 */
-	public static void start(Activity activity, Class<?> cls, int enterAnim, int exitAnim) {
-		start(activity, null, cls, null, REQUEST_CODE_INVALID, enterAnim, exitAnim);
+	public static void start(Activity activity, Class<?> cls, int enterAnim,
+			int exitAnim) {
+		start(activity, null, cls, null, REQUEST_CODE_INVALID, enterAnim,
+				exitAnim);
 	}
+
 	/**
 	 * Activity启动Activity(动画效果+携带数据)
 	 * 
@@ -141,10 +162,12 @@ public class Start {
 	 * @param exitAnim
 	 *            原Activity退出动画
 	 */
-	public static void start(Activity activity, Class<?> cls, Bundle extras, int enterAnim,
-			int exitAnim) {
-		start(activity, null, cls, extras, REQUEST_CODE_INVALID, enterAnim, exitAnim);
+	public static void start(Activity activity, Class<?> cls, Bundle extras,
+			int enterAnim, int exitAnim) {
+		start(activity, null, cls, extras, REQUEST_CODE_INVALID, enterAnim,
+				exitAnim);
 	}
+
 	/**
 	 * Activity启动Activity(动画效果+forResult)
 	 * 
@@ -159,10 +182,11 @@ public class Start {
 	 * @param exitAnim
 	 *            原Activity退出动画
 	 */
-	public static void start(Activity activity, Class<?> cls, int reqCode, int enterAnim,
-			int exitAnim) {
+	public static void start(Activity activity, Class<?> cls, int reqCode,
+			int enterAnim, int exitAnim) {
 		start(activity, null, cls, null, reqCode, enterAnim, exitAnim);
 	}
+
 	/**
 	 * Activity启动Activity(动画效果+携带数据+forResult)
 	 * 
@@ -177,10 +201,11 @@ public class Start {
 	 * @param exitAnim
 	 *            原Activity退出动画
 	 */
-	public static void start(Activity activity, Class<?> cls, Bundle extras, int reqCode,
-			int enterAnim, int exitAnim) {
+	public static void start(Activity activity, Class<?> cls, Bundle extras,
+			int reqCode, int enterAnim, int exitAnim) {
 		start(activity, null, cls, extras, reqCode, enterAnim, exitAnim);
 	}
+
 	/**
 	 * Activity跳转方法
 	 * 
@@ -199,8 +224,9 @@ public class Start {
 	 * @param exitAnim
 	 *            原Activity退出动画
 	 */
-	public static void start(Activity activity, Fragment fragment, Class<?> cls, Bundle extras,
-			int reqCode, int enterAnim, int exitAnim) {
+	public static void start(Activity activity, Fragment fragment,
+			Class<?> cls, Bundle extras, int reqCode, int enterAnim,
+			int exitAnim) {
 		if (null != activity) {
 			Intent intent = new Intent(activity, cls);
 			if (null != extras) {
