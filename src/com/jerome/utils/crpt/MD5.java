5e123af275800493a485cd24bc3afb55bad90b61
jerome <songzhiyong1121@gmail.com>
2013-06-06 13:13:47 +0800

    md5

--- src/com/jerome/utils/crpt/MD5.java
+++ src/com/jerome/utils/crpt/MD5.java
@@ -14,7 +14,7 @@ public class MD5 {
 			md5 = MessageDigest.getInstance("MD5");
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "";
+			return null;
 		}
 		char[] charArray = str.toCharArray();
 		byte[] byteArray = new byte[charArray.length];
@@ -25,12 +25,16 @@ public class MD5 {
 		byte[] md5Bytes = md5.digest(byteArray);
 
 		StringBuffer hexValue = new StringBuffer();
+		// 写法忒不专业
+		// for (int i = 0; i < md5Bytes.length; i++) {
+		// int val = ((int) md5Bytes[i]) & 0xff;
+		// if (val < 16) {
+		// hexValue.append("0");
+		// }
+		// hexValue.append(Integer.toHexString(val));
+		// }
 		for (int i = 0; i < md5Bytes.length; i++) {
-			int val = ((int) md5Bytes[i]) & 0xff;
-			if (val < 16) {
-				hexValue.append("0");
-			}
-			hexValue.append(Integer.toHexString(val));
+			hexValue.append(String.format("%02x", ((int) md5Bytes[i]) & 0xff));
 		}
 		return hexValue.toString();
 	}
