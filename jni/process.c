148a2af53caaf599d0bd8ea13a6aa05af0657e8d
jerome <songzhiyong1121@gmail.com>
2013-06-07 10:37:10 +0800

    util

--- jni/process.c
+++ jni/process.c
@@ -10,27 +10,27 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 /**
- * ¹¤¾ß·½·¨
- * ·µ»ØÖµ char* Õâ¸ö´ú±ícharÊý×éµÄÊ×µØÖ·
- *  Jstring2CStr °ÑjavaÖÐµÄjstringµÄÀàÐÍ×ª»¯³ÉÒ»¸öcÓïÑÔÖÐµÄchar ×Ö·û´®
+ * å·¥å…·æ–¹æ³•
+ * è¿”å›žå€¼ char* è¿™ä¸ªä»£è¡¨charæ•°ç»„çš„é¦–åœ°å€
+ *  Jstring2CStr æŠŠjavaä¸­çš„jstringçš„ç±»åž‹è½¬åŒ–æˆä¸€ä¸ªcè¯­è¨€ä¸­çš„char å­—ç¬¦ä¸²
  */
-char*   Jstring2CStr(JNIEnv*   env,   jstring   jstr)
-{
-	 char*   rtn   =   NULL;
-	 jclass   clsstring   =   (*env)->FindClass(env,"java/lang/String");
-	 jstring   strencode   =   (*env)->NewStringUTF(env,"GB2312");
-	 jmethodID   mid   =   (*env)->GetMethodID(env,clsstring,   "getBytes",   "(Ljava/lang/String;)[B");
-	 jbyteArray   barr=   (jbyteArray)(*env)->CallObjectMethod(env,jstr,mid,strencode); 
-	 jsize   alen   =   (*env)->GetArrayLength(env,barr);
-	 jbyte*   ba   =   (*env)->GetByteArrayElements(env,barr,JNI_FALSE);
-	 if(alen   >   0)
-	 {
-	  rtn   =   (char*)malloc(alen+1);         
-	  memcpy(rtn,ba,alen);
-	  rtn[alen]=0;
-	 }
-	 (*env)->ReleaseByteArrayElements(env,barr,ba,0);  
-	 return rtn;
+char* Jstring2CStr(JNIEnv* env, jstring jstr) {
+	char* rtn = NULL;
+	jclass clsstring = (*env)->FindClass(env, "java/lang/String");
+	jstring strencode = (*env)->NewStringUTF(env, "GB2312");
+	jmethodID mid = (*env)->GetMethodID(env, clsstring, "getBytes",
+			"(Ljava/lang/String;)[B");
+	jbyteArray barr = (jbyteArray)(*env)->CallObjectMethod(env, jstr, mid,
+			strencode);
+	jsize alen = (*env)->GetArrayLength(env, barr);
+	jbyte* ba = (*env)->GetByteArrayElements(env, barr, JNI_FALSE);
+	if (alen > 0) {
+		rtn = (char*) malloc(alen + 1);
+		memcpy(rtn, ba, alen);
+		rtn[alen] = 0;
+	}
+	(*env)->ReleaseByteArrayElements(env, barr, ba, 0);
+	return rtn;
 }
 
 /*
@@ -38,17 +38,16 @@ char*   Jstring2CStr(JNIEnv*   env,   jstring   jstr)
  * Method:    getInfo
  * Signature: (Ljava/lang/String;I)Ljava/lang/String;
  */
-JNIEXPORT jstring JNICALL Java_com_jerome_jni_JNIProcess_getInfo
-  (JNIEnv * env, jclass jcls, jstring jname, jint jage)
-{
-	char* cname = Jstring2CStr(env,jname);
-	char hello[7] = {'-','H','e','l','l','o','\0'};
-	strcat(cname,hello);
+JNIEXPORT jstring JNICALL Java_com_jerome_jni_JNIProcess_getInfo(JNIEnv * env,
+		jclass jcls, jstring jname, jint jage) {
+	char* cname = Jstring2CStr(env, jname);
+	char hello[7] = { '-', 'H', 'e', 'l', 'l', 'o', '\0' };
+	strcat(cname, hello);
 	char age[10];
 	int cage = jage;
 	//itoa(cage,age,10);
 	//strcat(cname,age);
-	return (*env)->NewStringUTF(env,cname);
+	return (*env)->NewStringUTF(env, cname);
 }
 
 /*
@@ -56,6 +55,6 @@ JNIEXPORT jstring JNICALL Java_com_jerome_jni_JNIProcess_getInfo
  * Method:    getReverseByte
  * Signature: (B)I
  */
-JNIEXPORT jint JNICALL Java_com_jerome_jni_JNIProcess_getReverseByte
-	(JNIEnv * env, jclass jcls, jbyte b){
+JNIEXPORT jint JNICALL Java_com_jerome_jni_JNIProcess_getReverseByte(
+		JNIEnv * env, jclass jcls, jbyte b) {
 }
